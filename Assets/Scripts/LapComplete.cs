0755e02270e589d5a3f529a0e359d98ddca85302
Kosim <kelfian@binus.ac.id>
2018-05-10 12:49:51 +0700

    Without Unnecessary Files.

---
+++ Assets/Scripts/LapComplete.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LapComplete : MonoBehaviour {
+
+	public GameObject lapCompleteTrig;
+	public GameObject halfLapTrig;
+
+	public TextMeshProUGUI lapCounter;
+    public TextMeshProUGUI bestTimeBox;
+
+    [HideInInspector]
+    public float bestRawTime;
+    public static int LapsDone;
+
+	private void OnTriggerEnter()
+    {
+		LapsDone += 1;
+		bestRawTime = PlayerPrefs.GetFloat ("RawTime");
+		if (TimeManager.rawTime <= bestRawTime)
+        {
+            bestTimeBox.text = string.Format("{0:00}:", TimeManager.minuteCount) + string.Format("{0:00}.", TimeManager.secondCount) + TimeManager.miliDisplay;
+        }
+
+		PlayerPrefs.SetInt ("MinSave", TimeManager.minuteCount);
+		PlayerPrefs.SetInt ("SecSave", TimeManager.secondCount);
+		PlayerPrefs.SetFloat ("MiliSave", TimeManager.miliCount);
+		PlayerPrefs.SetFloat ("RawTime", TimeManager.rawTime);
+
+		TimeManager.minuteCount = 0;
+		TimeManager.secondCount = 0;
+		TimeManager.miliCount = 0;
+		TimeManager.rawTime = 0;
+        
+        lapCounter.text = "" + LapsDone;
+
+		halfLapTrig.SetActive (true);
+		lapCompleteTrig.SetActive (false);
+	}
+
+}
---
+++ Assets/Scripts/VController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Vehicle : System.Object  
+{
+	public WheelCollider leftWheel; // masukin collider ban
+	public GameObject leftWheelMesh; // masukin model ban
+	public WheelCollider rightWheel; // masukin collider ban
+	public GameObject rightWheelMesh; // masukin model ban
+	public bool motor; // main engine
+	public bool steering; // bisa belok apa enggak
+	public bool reverseTurn; // reverse controller
+}
+
+public class VController : MonoBehaviour {
+
+	public float maxMotorTorque; // horse power
+	public float maxSteeringAngle; // puteran steernya berapa derajat
+	public List<Vehicle> vehicleWheels; // masukin ban nya ada berapa pasang
+
+	public void VisualizeWheel(Vehicle wheelPair)
+	{
+		Quaternion rot;
+		Vector3 pos;
+		wheelPair.leftWheel.GetWorldPose ( out pos, out rot);
+		wheelPair.leftWheelMesh.transform.position = pos;
+		wheelPair.leftWheelMesh.transform.rotation = rot;
+		wheelPair.rightWheel.GetWorldPose ( out pos, out rot);
+		wheelPair.rightWheelMesh.transform.position = pos;
+		wheelPair.rightWheelMesh.transform.rotation = rot;
+	}
+
+	public void Update()
+	{
+		float motor = maxMotorTorque * Input.GetAxis("Vertical");
+		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+		float brakeTorque = Mathf.Abs(Input.GetAxis("Jump"));
+		if (brakeTorque > 0.001) {
+			brakeTorque = maxMotorTorque;
+			motor = 0;
+		} else {
+			brakeTorque = 0;
+		}
+
+		foreach (Vehicle vehicleWheel in vehicleWheels)
+		{
+			if (vehicleWheel.steering == true) {
+				vehicleWheel.leftWheel.steerAngle = vehicleWheel.rightWheel.steerAngle = ((vehicleWheel.reverseTurn)?-1:1)*steering;
+			}
+
+			if (vehicleWheel.motor == true)
+			{
+				vehicleWheel.leftWheel.motorTorque = motor;
+				vehicleWheel.rightWheel.motorTorque = motor;
+			}
+
+			vehicleWheel.leftWheel.brakeTorque = brakeTorque;
+			vehicleWheel.rightWheel.brakeTorque = brakeTorque;
+
+			VisualizeWheel(vehicleWheel);
+		}
+
+	}
+		
+}
