710b162cdafb87cc59a16fe1c16c6945ecc8dfab
Kosim <kelfian@binus.ac.id>
2018-05-13 12:58:18 +0700

    Checkpoint

--- Assets/SpeedoMeter.cs
+++ Assets/SpeedoMeter.cs
@@ -1,18 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
 
 public class SpeedoMeter : MonoBehaviour
 {
-    public Rigidbody rb;
-    private float speed = 0;
-    private Image arrow;
+    [SerializeField] private CarController carScript;
+    [SerializeField] private float minAngle = 82f;
+    [SerializeField] private float maxAngle = -82f;
+    [SerializeField] private float maxSpeed = 180f;
+    private Image arrow;    
 
-	private void Start ()
+    public float MinAngle { get; set; }
+    public float MaxAngle { get; set; }
+    public float MaxSpeed { get; set; }
+
+    private void Start ()
     {
         arrow = GetComponent<Image>();
     }
     private void LateUpdate ()
     {
-        arrow.rectTransform.eulerAngles = new Vector3(0, 0, Mathf.Lerp(82, -82, Mathf.InverseLerp(0, 100, rb.velocity.magnitude)));
-	}
+        arrow.rectTransform.eulerAngles = new Vector3(0, 0, Mathf.Lerp(minAngle, maxAngle, Mathf.InverseLerp(0, maxSpeed, carScript.CurrentSpeed)));
+    }
 }
