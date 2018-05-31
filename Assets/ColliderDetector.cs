1379ddad17ca1979e9812703f52a663ce1ec4b41
Kosim <kelfian@binus.ac.id>
2018-05-31 08:28:32 +0700

    AI Fixed

---
+++ Assets/ColliderDetector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ColliderDetector : MonoBehaviour
+{
+    private BoxCollider waypointCollider;
+    private void Start()
+    {
+        waypointCollider = GetComponent<BoxCollider>();
+    }
+    private IEnumerator OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "CarAI00")
+        {
+            waypointCollider.enabled = false;
+            AIWaypoint.ChangeWaypoint();
+            yield return new WaitForSeconds(1);
+            waypointCollider.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/AIWaypoint.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class AIWaypoint : MonoBehaviour
+{
+	private static GameObject gotoHere;
+    private static Transform[] waypoint;
+	private static int tracker = 0;
+    private static int totalPos = 0;
+
+    private void Awake()
+    {
+        gotoHere = transform.GetChild(0).gameObject;
+        totalPos = transform.childCount - 1;
+        Array.Resize(ref waypoint, totalPos);
+        for (int i = 1; i < transform.childCount; i++)
+        {
+            waypoint[i-1] = transform.GetChild(i);
+        }
+    }
+
+    private void Start()
+    {
+        gotoHere.transform.localPosition = waypoint[0].transform.localPosition;
+        
+    }
+    public static void ChangeWaypoint()
+    {
+        tracker++;
+        if (tracker == totalPos)
+        {
+            tracker = 0;
+        }
+        gotoHere.transform.localPosition = waypoint[tracker].localPosition;
+    }
+}
